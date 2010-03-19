a557a9516b7764b0ef3cc261fe6a0710fef838bc
fjo <fjo@fc4492ce-c460-4c54-a5dc-289448670530>
2010-03-19 14:43:53 +0000

    *** empty log message ***
    git-svn-id: https://svn-1.sys.kth.se/svn/vc/repos/vc/trunk/django-cas@1203 fc4492ce-c460-4c54-a5dc-289448670530

--- setup.py
+++ setup.py
@@ -37,5 +37,5 @@ to the admin interface.
     name='django_cas',
     packages=['django_cas'],
     url='http://code.google.com/p/django-cas/',
-    version='2.0.3-kth',
+    version='2.0.3-KTH-0',
 )
