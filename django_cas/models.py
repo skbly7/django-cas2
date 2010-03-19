ac637fab958be324e1949b5b3cacd9b7b7105274
fjo <fjo@fc4492ce-c460-4c54-a5dc-289448670530>
2010-03-19 09:55:27 +0000

    Added support for proxy authentication, requires addition of django_cas to installed apps and a syncdb to create tables to hold tickets
    git-svn-id: https://svn-1.sys.kth.se/svn/vc/repos/vc/trunk/django-cas@1177 fc4492ce-c460-4c54-a5dc-289448670530

--- django_cas/models.py
+++ django_cas/models.py
@@ -1,2 +1,45 @@
+from urlparse import urljoin
+from urllib import urlencode, urlopen
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
+
+class Tgt(models.Model):
+    username = models.CharField(max_length = 255, unique = True)
+    tgt = models.CharField(max_length = 255)
+
+    def get_proxy_ticket_for(self, service):
+        """Verifies CAS 2.0+ XML-based authentication ticket.
+
+        Returns username on success and None on failure.
+        """
+
+        try:
+            from xml.etree import ElementTree
+        except ImportError:
+            from elementtree import ElementTree
+
+        params = {'pgt': self.tgt, 'targetService': service}
+
+        url = (urljoin(settings.CAS_SERVER_URL, 'proxy') + '?' +
+               urlencode(params))
+
+        page = urlopen(url)
+
+        try:
+            response = page.read()
+            tree = ElementTree.fromstring(response)
+            if tree[0].tag.endswith('proxySuccess'):
+                return tree[0][0].text
+            else:
+                return None
+        finally:
+            page.close()
+
+class PgtIOU(models.Model):
+    pgtIou = models.CharField(max_length = 255, unique = True)
+    tgt = models.CharField(max_length = 255)
+    timestamp = models.DateTimeField(auto_now = True)
+
+def get_tgt_for(user):
+    return Tgt.objects.get(username = user.username)
