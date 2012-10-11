55d5129be475936f863532ffdc1b9b9548cb7319
Fredrik Jönsson <fjo@kth.se>
2012-10-11 10:55:11 +0200

    Refactorization, rewritten to high extent.

--- django_cas/views.py
+++ django_cas/views.py
@@ -1,49 +1,51 @@
 """CAS login/logout replacement views"""
 
+from django.conf import settings
+from django.contrib import auth
+from django.http import HttpResponseRedirect, HttpResponseForbidden, \
+    HttpResponse
+from django_cas.models import PgtIOU, SessionServiceTicket
 from urllib import urlencode
 from urlparse import urljoin
+from xml.dom import minidom
+import logging
 
-from django.http import get_host, HttpResponseRedirect, HttpResponseForbidden, HttpResponse
-from django.conf import settings
-from django.contrib.auth import REDIRECT_FIELD_NAME
-from django_cas.models import PgtIOU, SessionServiceTicket
+__all__ = ['login', 'logout', 'proxy_callback']
 
-__all__ = ['login', 'logout']
+logger = logging.getLogger(__name__)
+
+def _service(request):
+    """ Returns service host URL as derived from request """
+
+    return ('http://', 'https://')[request.is_secure()] + request.get_host()
+
 
 def _service_url(request, redirect_to=None):
-    """Generates application service URL for CAS"""
-
-    protocol = ('http://', 'https://')[request.is_secure()]
-    host = get_host(request)
-    service = protocol + host + request.path
-    if redirect_to:
-        if '?' in service:
-            service += '&'
-        else:
-            service += '?'
-        service += urlencode({REDIRECT_FIELD_NAME: redirect_to})
-    return service
+    """ Returns application service URL for CAS. """
+    
+    service = _service(request) + request.path
+    if not redirect_to:
+        return service
+    else:
+        return ''.join([service,
+                        '?' if not '?' in service else '&',
+                        urlencode({auth.REDIRECT_FIELD_NAME: redirect_to})])
 
 
 def _redirect_url(request):
-    """Redirects to referring page, or CAS_REDIRECT_URL if no referrer is
-    set.
-    """
-    redirect_to = request.GET.get(REDIRECT_FIELD_NAME)
-    if not redirect_to:
-        if settings.CAS_IGNORE_REFERER:
-            redirect_to = settings.CAS_REDIRECT_URL
-        else:
-            redirect_to = request.META.get('HTTP_REFERER', settings.CAS_REDIRECT_URL)
-        prefix = (('http://', 'https://')[request.is_secure()] +
-                  get_host(request))
-        if redirect_to.startswith(prefix):
-            redirect_to = next[len(prefix):]
-    return redirect_to
+    """ Redirects to referring page, or CAS_REDIRECT_URL if no referrer. """
+
+    if request.GET.get(auth.REDIRECT_FIELD_NAME):
+        return request.GET.get(auth.REDIRECT_FIELD_NAME)
+    
+    if settings.CAS_IGNORE_REFERER:
+        return settings.CAS_REDIRECT_URL
+
+    return request.META.get('HTTP_REFERER', settings.CAS_REDIRECT_URL)
 
 
 def _login_url(service):
-    """Generates CAS login URL"""
+    """ Returns a CAS login URL. """
 
     params = {'service': service}
     if settings.CAS_EXTRA_LOGIN_PARAMS:
@@ -52,70 +54,63 @@ def _login_url(service):
 
 
 def _logout_url(request, next_page=None):
-    """Generates CAS logout URL"""
+    """ Returns a CAS logout URL """
 
-    url = urljoin(settings.CAS_SERVER_URL, 'logout')
+    logout_url = urljoin(settings.CAS_SERVER_URL, 'logout')
     if next_page:
-        protocol = ('http://', 'https://')[request.is_secure()]
-        host = get_host(request)
-        url += '?' + urlencode({'url': protocol + host + next_page})
-    return url
+        logout_url += '?' + urlencode({'url': _service(request) + next_page})
+
+    return logout_url
 
 
 def login(request, next_page=None, required=False):
-    """Forwards to CAS login URL or verifies CAS ticket"""
+    """ Forwards to CAS login URL or verifies CAS ticket. """
 
     if not next_page:
         next_page = _redirect_url(request)
     if request.user.is_authenticated():
         return HttpResponseRedirect(next_page)
-    ticket = request.GET.get('ticket')
-    service = _service_url(request, next_page)
-    if ticket:
-        from django.contrib import auth
-        user = auth.authenticate(ticket=ticket, service=service)
 
-        if user is not None:
-            auth.login(request, user)
-            return HttpResponseRedirect(next_page)
-        elif settings.CAS_RETRY_LOGIN or required:
-            return HttpResponseRedirect(_login_url(service))
-        else:
-            error = "<h1>Forbidden</h1><p>Login failed.</p>"
-            return HttpResponseForbidden(error)
-    else:
+    service = _service_url(request, next_page)
+    ticket = request.GET.get('ticket')
+    if not ticket:
+        return HttpResponseRedirect(_login_url(service))
+       
+    user = auth.authenticate(ticket=ticket, service=service)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect(next_page)
+    
+    if settings.CAS_RETRY_LOGIN or required:
         return HttpResponseRedirect(_login_url(service))
 
+    return HttpResponseForbidden("<h1>Forbidden</h1><p>Login failed.</p>")
+ 
 
-def _get_session(samlp):
+def _get_session(logout_response):
     """ Recovers the session mapped with the CAS service ticket
-    received in the SAML CAS request at CAS logout
+        received in the SAML CAS response at CAS logout.
     """
     try:
-        from xml.etree import ElementTree as ET
-    except ImportError:
-        from elementtree import ElementTree as ET
-
-    try:
-        tree = ET.fromstring(samlp)
-        if tree[1].tag.endswith('SessionIndex'):
-            ticket = tree[1].text
+        response = minidom.parseString(logout_response)
+        ticket = response.getElementsByTagName('samlp:SessionIndex')[0].firstChild.nodeValue
         sst = SessionServiceTicket.objects.get(pk=ticket)
-    except:
+        return sst.get_session()
+    except Exception as e:
+        logger.error("Unable to parse logout response from server: %s", e)
         return None
-    return sst.get_session()
-
 
 
 def logout(request, next_page=None):
-    """Redirects to CAS logout page"""
-    cas_logout_request = request.POST.get('logoutRequest', '')
-    if cas_logout_request:
-        session = _get_session(cas_logout_request)
-        request.session = session
+    """ Redirects to CAS logout page. """
 
-    from django.contrib.auth import logout
-    logout(request)
+    cas_logout_response = request.POST.get('logoutRequest', '')
+    if cas_logout_response:
+        request.session = _get_session(cas_logout_response)
+        logger.debug("Got single sign out callback from CAS server for session %s", request.session)
+
+    auth.logout(request)
     if not next_page:
         next_page = _redirect_url(request)
     if settings.CAS_LOGOUT_COMPLETELY:
@@ -123,14 +118,17 @@ def logout(request, next_page=None):
     else:
         return HttpResponseRedirect(next_page)
 
+
 def proxy_callback(request):
     """Handles CAS 2.0+ XML-based proxy callback call.
 
-    Stores the proxy granting ticket in the database for 
-    future use.
+        Stores the proxy granting ticket in the database for 
+        future use.
     """
     pgtIou = request.GET.get('pgtIou')
     tgt = request.GET.get('pgtId')
+
+    logger.debug("Got proxy callback from CAS server for pgt %s wiht pgtIou %s", tgt, pgtIou)
 
     if not (pgtIou and tgt):
         return HttpResponse()
